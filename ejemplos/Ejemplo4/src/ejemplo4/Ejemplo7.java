6d19cb998092c4a91b03a66b6e4b6643f1c7a62f
jhon pardo <jhonedupardo15@gmail.com>
2025-11-13 11:34:45 -0500

    placa

--- ejemplos/Ejemplo4/src/ejemplo4/Ejemplo7.java
+++ ejemplos/Ejemplo4/src/ejemplo4/Ejemplo7.java
@@ -1,7 +1,9 @@
 /*
- Realizar un programa en java que permita presentar un mensaje de: 
-acceso correcto, si el valor de la primera letra de la variable ciudad 
-tiene el valor de "L"; caso contrario, presentar un mensaje de acceso incorrecto
+El programa presenta mensaje de "acceso correcto para las placas de vehiculos de 
+la provincia del Guayas y Pichincha 
+LBA-0010
+GBC-0010
+GBZ-0101
  */
 package ejemplo4;
 
@@ -12,31 +14,32 @@ import java.util.Scanner;
  *
  * @author reroes
  */
-public class Ejemplo6 {
+public class Ejemplo7 {
 
     public static void main(String[] args) {
         // 
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
-        System.out.println("Ingrese la ciudad");
+        System.out.println("Ingrese la placa");
         String ciudad = entrada.nextLine();
 
-        // L    0
-        // O    1
-        // J    2 
-        // A    3
-        // 
+        // G   0        
+        // U   1
+        // A   2 
+        // Y   3
+        // A   4
+        // S   5
         String inicial = ciudad.substring(0, 1); // se obtiene 
         // el primer 
         // caracter de la cadena
         // Si el valor de ciudad es 
-        // "Loja" se obtiene "L"
+        // "Guayas" se obtiene "G"
 
-        if ( inicial.equals("L") || inicial.equals("l")) {
+       if ( inicial.equals("G") || inicial.equals("g")
+                || inicial.equals("P")|| inicial.equals("p"))  {
             System.out.printf("%s\n", "acceso correcto");
         } else {
             System.out.printf("%s\n", "acceso incorrecto");
-            
         }
 
     }
